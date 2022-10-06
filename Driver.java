a91c71040ba5a7f76bd3caf0ff5874276f691fc1
unknown <vatra.lestrange@gmail.com>
2022-10-05 23:37:14 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,50 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1 = {6, 3};
+        int [] e1 = {2, 5};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double [] c2 = {8, 7, 16};
+        int [] e2 = {3, 2, 5};
+		Polynomial p2 = new Polynomial(c2, e2);
+
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+
+        // for(int i = 0; i < 12; i++){
+        //     System.out.println("coeff: " + s.arr_coeff[i] + "\n");
+        //     System.out.println("exp: " + s.arr_exp[i] + "\n\n\n");
+        // }
+
+        Polynomial m = p1.multiply(p2);
+        System.out.println("m(0.1) = " + m.evaluate(0.1) + "\n");
+        for(int i = 0; i < 8; i++){
+            System.out.println("coeff: " + m.arr_coeff[i]);
+            System.out.println("exp: " + m.arr_exp[i] + "\n\n\n");
+        }
+
+        //File f = new File("Test.txt");
+		  //Polynomial confusion = new Polynomial(f);
+
+
+		// if(s.hasRoot(1))
+		// 	System.out.println("1 is a root of s");
+		// else
+		// 	System.out.println("1 is not a root of s");
+
+        // double [] c1 = {6, 7, 5};
+        // int [] e1 = {5, 3, 2};
+        // double [] c2 = {8, 2, 0, 7, 4};
+        // int [] e2 = {0, 1, 0, 4, 2};
+        // Polynomial p1 = new Polynomial(c1, e1);
+        // Polynomial p2 = new Polynomial(c2, e2);
+
+        // Polynomial s = p1.add(p2);
+		// System.out.println("s(2.0) = " + s.evaluate(2.0));
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,29 +1,90 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class Polynomial{
-	double[] arr_coeff = new double[100];
+	double[] arr_coeff = new double[100];   //coefficients
+    int[] arr_exp = new int[100];           //exponents or power
 
 	public Polynomial(){
 		for (int i = 0; i < 100; i++){
 			arr_coeff[i] = 0;
+            arr_exp[i] = 0;
 		}
 	}
 
-	public Polynomial(double[] arr){
-		int arr_size = arr.length;
-		for (int i = 0; i < arr_size; i++){
-			this.arr_coeff[i] = arr[i];
+	public Polynomial(double[] c, int[] e){
+		int coeff_size = c.length;
+		for (int i = 0; i < coeff_size; i++){
+			this.arr_coeff[i] = c[i];
 		}
-		if (arr_size < 100){
-			for (int i = 0; i < arr_size; i++){
-				this.arr_coeff[i] = arr[i];
+		if (coeff_size < 100){
+			for (int i = coeff_size; i < 100; i++){
+				this.arr_coeff[i] = 0;
+			}
+		}
+
+        int exp_size = e.length;
+		for (int i = 0; i < exp_size; i++){
+			this.arr_exp[i] = e[i];
+		}
+		if (exp_size < 100){
+			for (int i = exp_size; i < 100; i++){
+				this.arr_exp[i] = 0;
 			}
 		}
 	}
 
-	public Polynomial add(Polynomial arg){
+	// public Polynomial(File f){
+	// 	BufferedReader br = new BufferedReader(f);
+	// 	Scanner
+ 	// 	String str = br.readLine();
+    //     System.out.println(str);
+    // }
+
+	public Polynomial add (Polynomial arg){
 		Polynomial temp = new Polynomial();
-		for  (int i = 0; i < 100; i++){
-			temp.arr_coeff[i] = this.arr_coeff[i] + arg.arr_coeff[i];
-		}
+        boolean found = false;
+        int space = 0;
+        
+        for (int i = 0; i < 100; i++){  //copy values to temp
+            if (this.arr_coeff[i] != 0){
+                temp.arr_coeff[space] = this.arr_coeff[i];
+                temp.arr_exp[space] = this.arr_exp[i];
+                space++;
+            }
+        }
+
+        for (int i = 0; i < 100; i++){  //add to temp is exp is in temp
+            found = false;
+            if(temp.arr_coeff[i] != 0){
+                // System.out.println("here\n");
+                for (int j = 0; j < 100; j++){
+                    if ((found == false) & (temp.arr_exp[i] == arg.arr_exp[j])){
+                        // System.out.println("b\n");
+                        found = true;
+                        temp.arr_coeff[i] += arg.arr_coeff[j];
+                        // System.out.println("c\t" + temp.arr_coeff[i] + "\n");
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < 100; i++){       //loop through arg, if exp is not in temp, add into temp
+            found = false;
+            if(arg.arr_coeff[i] != 0){
+                for (int j = 0; j < 100; j++){
+                    if((found == false) & (arg.arr_exp[i] == temp.arr_exp[j])){found = true;}
+                }
+
+                if (found == false){
+                    temp.arr_coeff[space] = arg.arr_coeff[i];
+                    temp.arr_exp[space] = arg.arr_exp[i];
+                    space++;
+                }
+            }
+        }
+
 		return temp;
 	}
 
@@ -32,7 +93,7 @@ public class Polynomial{
 		double temp = 1;
 		for (int i = 0; i < 100; i++){
 			temp = 1;
-			for (int j = 0; j < i; j++){temp *= x;}
+			for (int j = 0; j < this.arr_exp[i]; j++){temp *= x;}     //power function
 			total += temp * this.arr_coeff[i];
 		}
 		return total;
@@ -43,17 +104,57 @@ public class Polynomial{
 		if (total == 0.0){return true;}
 		else {return false;}
 	}
+
+    public Polynomial multiply (Polynomial arg){
+        int temp = 0;       //storage for the current exponent of total Polynomial
+        Polynomial total = new Polynomial();
+        boolean found = false;      //for if exp is found or empty space is found
+        int counter = 0;
+        int space = 0;
+
+        for  (int i = 0; i < 100; i++){         //loops this
+            if(this.arr_coeff[i] != 0){
+                for  (int j = 0; j < 100; j++){         //loops arg
+                    if(arg.arr_coeff[j] != 0){
+                        temp = this.arr_exp[i] + arg.arr_exp[j];        //temporary or current exp
+                        found = false;
+
+                        //multiplication
+                        counter = 0;
+                        while ((found == false) & (counter < 100)){     //checks if there are redundant exponents
+                            if ((total.arr_exp[counter] == temp) & (total.arr_coeff[counter] != 0)){     //if there is a redundant exponent
+                                System.out.println("here\n");
+                                total.arr_coeff[counter] += this.arr_coeff[i] * arg.arr_coeff[j];
+                                found = true;
+                            }
+                            counter++;
+                        }
+
+                        //add new exponent
+                        if (found == false){        //exponent is not in total
+                            total.arr_coeff[space] = this.arr_coeff[i] * arg.arr_coeff[j];
+                            total.arr_exp[space] = this.arr_exp[i] + arg.arr_exp[j];
+                            space++;
+                        }
+                    }
+                }
+            }
+        }
+        return total;
+    }
+
+    public String saveToFile (String str){
+        String fin = "";
+        int i = 0;
+
+        fin = fin + Double.toString(this.arr_coeff[i]) + "x" + Integer.toString(this.arr_exp[i]); i++;
+        while ((this.arr_coeff[i] != 0) && (this.arr_coeff[i + 1] != 0) && ((this.arr_exp[i] != 0) && (this.arr_exp[i + 1] != 0))){
+            if (this.arr_coeff[i] < 0){fin = fin + "+";}
+            fin = fin + Double.toString(this.arr_coeff[i]) + "x" + Integer.toString(this.arr_exp[i]);
+            i++;
+        }
+
+        // FileWriter output = new FileWriter(str, false); //use true for appending
+		return fin;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
